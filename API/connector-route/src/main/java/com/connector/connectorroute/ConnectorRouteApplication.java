0220250be5797bae88e4ff6980287a27e0429e0f
Harigamer <harigamer932@gmail.com>
2023-09-06 09:57:08 +0530

    Initialized spring boot application

---
+++ API/connector-route/src/main/java/com/connector/connectorroute/ConnectorRouteApplication.java
@@ -0,0 +1,13 @@
+package com.connector.connectorroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectorRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectorRouteApplication.class, args);
+	}
+
+}
---
+++ API/connector-route/src/test/java/com/connector/connectorroute/ConnectorRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connector.connectorroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectorRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
