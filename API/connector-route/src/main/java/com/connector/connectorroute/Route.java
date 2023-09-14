1cb4ab07a068364a349e20d7d4f4a6d38cbe4a8f
Harigamer <harigamer932@gmail.com>
2023-09-14 17:07:40 +0530

    Modified the Route class to changing the targe to last node to -1

--- API/connector-route/src/main/java/com/connector/connectorroute/Route.java
+++ API/connector-route/src/main/java/com/connector/connectorroute/Route.java
@@ -1,17 +1,18 @@
 package com.connector.connectorroute;
 
 public class Route {
-    public int id;
-    public int incon;
+    public String id;
+    public String source;
     public String inmsg;
-    public int outcon;
+    public String target;
     public String outmsg;
 
-    public Route(int id, int incon, String inmsg, int outcon, String outmsg) {
+
+    public Route(String id,String source,String inmsg,String target,String outmsg) {
         this.id = id;
-        this.incon = incon;
-        this.inmsg = inmsg;
-        this.outcon = outcon;
-        this.outmsg = outmsg;
+        this.source=source;
+        this.target=target;
+        this.inmsg=inmsg;
+        this.outmsg=outmsg;
     }   
 }
--- API/connector-route/src/main/java/com/connector/connectorroute/RoutesController.java
+++ API/connector-route/src/main/java/com/connector/connectorroute/RoutesController.java
@@ -1,25 +1,33 @@
 package com.connector.connectorroute;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 public class RoutesController {
 
-    static final List<Route> routeList=Arrays.asList(
-        new Route(0,1,"A",2,"B"),
-        new Route(1,2,"B",3,"C"),
-        new Route(2,3,"C",4,"D"),
-        new Route(3,4,"D",5,"E"),
-        new Route(4,5,"E",6,"F")
+    static final List<Route> routeList = Arrays.asList(
+        new Route("route 0", "1", "A", "2", "B"),
+        new Route("route 1", "1", "A", "2", "C"),
+        new Route("route 2", "2", "B", "3", "D"),
+        new Route("route 3", "2", "C", "4", "E"),
+        new Route("route 4", "3", "D", "-1", "X"),
+        new Route("route 5", "4", "D", "-1", "X")
+
     );
 
     @GetMapping("/routes")
-    public List<Route> getRoutes(){
+    public List<Route> getRoutes() {
         return routeList;
     }
+
+    
 }
